481cda7b1d683d62062e4db259adfbcf7c3104ab
Jack Zylkin <jzylkin@DESKTOP-Q3DKPKE>
2025-03-26 01:31:23 -0700

    adding finder script

---
+++ finder-app/finder.sh
@@ -0,0 +1,28 @@
+set -u
+#set -x
+
+if [ $# -ne 2 ]
+then
+	>&2 echo "Wrong number of arguments. Specify two files"
+	exit 1
+fi
+
+DIRNAME=$1
+SEARCH_STRING=$2
+
+if [ -d $DIRNAME ]
+then
+	:
+else
+	>&2 echo "Directory $DIRNAME not found"
+	exit 1
+fi
+
+FILEMATCHES=$(grep $SEARCH_STRING ${DIRNAME}/* -o -i -s -m1 | wc -l) 
+LINEMATCHES=$(grep $SEARCH_STRING ${DIRNAME}/* -o -i -s | wc -l)
+
+echo The number of files are $FILEMATCHES and the number of matching lines are $LINEMATCHES
+
+
+
+
