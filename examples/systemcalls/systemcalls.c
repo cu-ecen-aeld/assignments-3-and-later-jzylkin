6adb0f100e121c6de19d17636e9be50b0af36de3
jzylkin <jzylkin@gmail.com>
2025-03-27 02:30:09 -0700

    test now passes in qemu

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -10,33 +10,33 @@
  *   successfully using the system() call, false if an error occurred,
  *   either in invocation of the system() call, or if a non-zero return
  *   value was returned by the command issued in @param cmd.
-*/
+ */
 bool do_system(const char *cmd)
 {
 
-/*
- *  Call the system() function with the command set in the cmd
- *   and return a boolean true if the system() call completed with success
- *   or false() if it returned a failure
-*/
+    /*
+     *  Call the system() function with the command set in the cmd
+     *   and return a boolean true if the system() call completed with success
+     *   or false() if it returned a failure
+     */
 
     int retval = system(cmd);
     return (retval >= 0);
 }
 
 /**
-* @param count -The numbers of variables passed to the function. The variables are command to execute.
-*   followed by arguments to pass to the command
-*   Since exec() does not perform path expansion, the command to execute needs
-*   to be an absolute path.
-* @param ... - A list of 1 or more arguments after the @param count argument.
-*   The first is always the full path to the command to execute with execv()
-*   The remaining arguments are a list of arguments to pass to the command in execv()
-* @return true if the command @param ... with arguments @param arguments were executed successfully
-*   using the execv() call, false if an error occurred, either in invocation of the
-*   fork, waitpid, or execv() command, or if a non-zero return value was returned
-*   by the command issued in @param arguments with the specified arguments.
-*/
+ * @param count -The numbers of variables passed to the function. The variables are command to execute.
+ *   followed by arguments to pass to the command
+ *   Since exec() does not perform path expansion, the command to execute needs
+ *   to be an absolute path.
+ * @param ... - A list of 1 or more arguments after the @param count argument.
+ *   The first is always the full path to the command to execute with execv()
+ *   The remaining arguments are a list of arguments to pass to the command in execv()
+ * @return true if the command @param ... with arguments @param arguments were executed successfully
+ *   using the execv() call, false if an error occurred, either in invocation of the
+ *   fork, waitpid, or execv() command, or if a non-zero return value was returned
+ *   by the command issued in @param arguments with the specified arguments.
+ */
 
 bool do_exec(int count, ...)
 {
@@ -53,62 +53,58 @@ bool do_exec(int count, ...)
     // and may be removed
     command[count] = command[count];
 
-/*
- * TODO:
- *   Execute a system command by calling fork, execv(),
- *   and wait instead of system (see LSP page 161).
- *   Use the command[0] as the full path to the command to execute
- *   (first argument to execv), and use the remaining arguments
- *   as second argument to the execv() command.
- *
- *  
-*/
+    /*
+     * TODO:
+     *   Execute a system command by calling fork, execv(),
+     *   and wait instead of system (see LSP page 161).
+     *   Use the command[0] as the full path to the command to execute
+     *   (first argument to execv), and use the remaining arguments
+     *   as second argument to the execv() command.
+     *
+     *  
+     */
     int pid = fork();
     if (pid < 0)
     {
-	    printf("failed to fork");
-	         va_end(args);
-		 return false;
+        printf("failed to fork");
+        va_end(args);
+        return false;
     }
     else if (pid == 0)
     {
         // Running as child
         if (execv(command[0], &command[0]) < 0)
-	{
-	    exit(1);
-	}
+        {
+            exit(1);
+        }
     }
     else
     {
-	int status;
+        int status;
         if (waitpid(pid, &status, 0) < 0)
-	{
-	     va_end(args);
-	     return false;
-	}
-	
-	if ( WIFEXITED(status) )
-	{
-		int es = WEXITSTATUS(status);
-		if (es !=0 )
-		{
-			return false;
-		}
-	}
+        {
+            va_end(args);
+            return false;
+        }
+
+        if ( WIFEXITED(status) && WEXITSTATUS(status) != 0)
+        {
+            return false;
+        }
 
     }
 
     va_end(args);
-    
-    
+
+
     return true;
 }
 
 /**
-* @param outputfile - The full path to the file to write with command output.
-*   This file will be closed at completion of the function call.
-* All other parameters, see do_exec above
-*/
+ * @param outputfile - The full path to the file to write with command output.
+ *   This file will be closed at completion of the function call.
+ * All other parameters, see do_exec above
+ */
 bool do_exec_redirect(const char *outputfile, int count, ...)
 {
     va_list args;
@@ -125,23 +121,23 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
     command[count] = command[count];
 
 
-/*
- * TODO
- *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
- *   redirect standard out to a file specified by outputfile.
- *   The rest of the behaviour is same as do_exec()
- *
-*/
+    /*
+     * TODO
+     *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
+     *   redirect standard out to a file specified by outputfile.
+     *   The rest of the behaviour is same as do_exec()
+     *
+     */
     int fd = open(outputfile, O_WRONLY);
     if (fd > 0)
     {
-    	dup2(fd, 1);
+        dup2(fd, 1);
     }
     else
     {
-	printf("could not open redirect file");
-	va_end(args);
-	return false;
+        printf("could not open redirect file");
+        va_end(args);
+        return false;
     }
 
     execv(command[0], command);
--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -12,6 +12,7 @@ BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
+ARM_TOOLCHAIN_PATH=$(dirname $(which ${CROSS_COMPILE}gcc))/../
 
 if [ $# -lt 1 ]
 then
@@ -35,9 +36,20 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    make -j4  ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
+    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
 fi
+cd ${OUTDIR}/linux-stable
 
 echo "Adding the Image in outdir"
+
+if [ ! -e ${OUTDIR}/Image ]
+then
+    ln -s ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}/Image 
+fi
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -47,7 +59,12 @@ then
     sudo rm  -rf ${OUTDIR}/rootfs
 fi
 
-# TODO: Create necessary base directories
+# Create necessary base directories
+mkdir rootfs
+cd rootfs
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -55,26 +72,51 @@ then
 git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
+    # Configure busybox
+    make distclean
+    make defconfig
 else
     cd busybox
 fi
 
-# TODO: Make and install busybox
+# Make and install busybox
 
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install CONFIG_PREFIX=${OUTDIR}/rootfs/
+
+    cd ${OUTDIR}/rootfs
+    
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
-# TODO: Add library dependencies to rootfs
+# Add library dependencies to rootfs
+cd ${ARM_TOOLCHAIN_PATH}
+cd */libc
+cp lib64/libc.so.6 ${OUTDIR}/rootfs/lib64/
+cp lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64/
+cp lib64/libm.so.6 ${OUTDIR}/rootfs/lib64/
+cp lib/ld-linux* ${OUTDIR}/rootfs/lib/
 
-# TODO: Make device nodes
+# Make device nodes
+cd ${OUTDIR}/rootfs/
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 666 dev/console c 5 1
 
-# TODO: Clean and build the writer utility
+# Clean and build the writer utility
+cd ${FINDER_APP_DIR}
+make clean
+make CROSS_COMPILER=aarch64-none-linux-gnu- all
 
-# TODO: Copy the finder related scripts and executables to the /home directory
+# Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cp * -R --dereference ${OUTDIR}/rootfs/home
 
-# TODO: Chown the root directory
+# Chown the root directory
+cd ${OUTDIR}/rootfs/
+sudo chown root:root .
 
-# TODO: Create initramfs.cpio.gz
+# Create initramfs.cpio.gz
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+cd ${OUTDIR}
+gzip -f initramfs.cpio
